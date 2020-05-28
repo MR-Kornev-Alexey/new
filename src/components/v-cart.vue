622d9eb1c0f6e4a45256b1e8404dc58aaab43f73
Ваше Имя <you@example.com>
2020-05-28 18:46:16 +0200

    28 05 add cart

--- src/components/v-cart.vue
+++ src/components/v-cart.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="v-cart">
+    <router-link :to="{name: 'catalog'}">
+      <div class="v-catalog__link_to_cart">Back to Catalog</div>
+    </router-link>
     <h2>Cart</h2>
+    <p v-if="!cart_data.length">There are no products in cart...</p>
     <v-cart-item
       v-for="(item,index) in cart_data"
       :key="item.article"
@@ -38,6 +42,15 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss">
+
+  .v-catalog__link_to_cart {
+    position: fixed;
+    top: 80px;
+    right: 10px;
+    padding: $padding*2;
+    border: solid 1px #aeaeae;
+    background: #ffffff;
+  }
 
 </style>
--- src/components/v-main-wrapper.vue
+++ src/components/v-main-wrapper.vue
@@ -1,35 +1,23 @@
 <template>
   <div class="v-main-wrapper">
-    <v-catalog/>
-    <v-cart
-      v-if="CART.length"
-      :cart_data="CART"
-    />
+    <keep-alive>
+      <router-view></router-view>
+    </keep-alive>
   </div>
 </template>
 
 <script>
-import vCatalog from './v-catalog'
-import vCart from './v-cart'
-import { mapGetters } from 'vuex'
 export default {
   name: 'v-main-wrapper',
-  components: {
-    vCatalog,
-    vCart
-  },
-  computed: {
-    ...mapGetters([
-      'CART'
-    ])
-  }
+  components: {},
+  computed: {}
 }
 </script>
 
 <style scoped lang="scss">
-.v-main-wrapper {
-  max-width: 900px;
-  margin: 0 auto;
-  text-align: center;
- }
+  .v-main-wrapper {
+    max-width: 900px;
+    margin: 0 auto;
+    text-align: center;
+  }
 </style>
