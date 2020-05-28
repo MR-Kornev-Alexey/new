1f616838a5209b4d3687e50b5423e7a60bc03308
Ваше Имя <you@example.com>
2020-05-28 09:05:56 +0200

    28 05 axios complit

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import store from './store'
+import store from './vuex/store'
 import './assets/styles/styles.scss'
 
 Vue.config.productionTip = false
