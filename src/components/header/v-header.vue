eae3841809f901cc37a8b0d54cd3ceba5c617724
Ваше Имя <you@example.com>
2020-05-31 20:39:50 +0200

    31 05 menu

--- src/components/header/v-header.vue
+++ src/components/header/v-header.vue
@@ -2,9 +2,10 @@
   <header class="v-header">
     <b-row align-v="center">
       <b-col><h1>Клевер линк</h1></b-col>
-      <b-col  cols="12" md="auto">Manager</b-col>
+      <b-col  cols="10" md="auto">Manager</b-col>
       <b-col col lg="1"> <img class="avatar" src="../../assets/images/avatarka.jpg" alt="Avatarka"></b-col>
     </b-row>
+    <hr>
   </header>
 </template>
 
@@ -16,13 +17,22 @@ export default {
 
 <style scoped lang="scss">
   .v-header {
+    hr{
+      border-bottom: 1px solid #3d498c;
+      width: 80%;
+    }
+
     margin: 0 auto;
     h1 {
+      font-size: 50px;
       text-transform: uppercase;
-      margin-left: 40px;
+      margin-left: 60px;
+      margin-top: 28px ;
+      margin-bottom: 12px;
     }
   }
   .avatar {
+    margin-right: 100px;
     height: 40px;
     width: 40px;
   }
--- src/components/sidebar/v-sidebar.vue
+++ src/components/sidebar/v-sidebar.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="s-container" :class="{'show': showSidebar}">
+    <div class="logo">
+      <img class="logo__img " src="../../assets/images/logo.png" alt="Avatarka">
+      <hr>
+    </div>
 
-    <div class="navigation-icons">
-      <i class="fas fa-home"></i>
-      <i class="fas fa-camera-retro"></i>
-      <i class="fas fa-user-circle"></i>
-      <i class="fas fa-comment-dots"></i>
-      <i class="fas fa-info-circle"></i>
-    </div>
-    <div class="navigation-links">
-      <transition-group name="fade">
-        <div v-show="showLink" key="1">Home</div>
-        <div v-show="showLink" key="2">Portfolio</div>
-        <div v-show="showLink" key="3">About Me</div>
-        <div v-show="showLink" key="4">Contact</div>
-        <div v-show="showLink" key="5">FAQ</div>
-      </transition-group>
-    </div>
-    <div class="control">
-      <i class="fas fa-angle-double-right" @click="showNav"></i>
-    </div>
+  <div class="control">
+    <i class="fas fa-angle-double-right" @click="showNav"></i>
+  </div>
+  <div class="navigation-icons">
+    <i class="fas fa-fw fa-tachometer-alt"></i>
+    <i class="fas fa-download"></i>
+    <i class="far fa-list-alt"></i>
+    <i class="fas fa-sort"></i>
+    <i class="far fa-trash-alt"></i>
+    <i class="far fa-chart-bar"></i>
+  </div>
+  <div class="navigation-links">
+    <transition-group name="fade">
+      <div v-show="showLink" key="1">Панель</div>
+      <div v-show="showLink" key="2">Ввод</div>
+      <div v-show="showLink" key="3">База</div>
+      <div v-show="showLink" key="4">Рейтинг</div>
+      <div v-show="showLink" key="5">Удаление</div>
+      <div v-show="showLink" key="6">Статистика</div>
+    </transition-group>
+  </div>
   </div>
 </template>
 
@@ -54,36 +60,49 @@ export default {
     position: absolute;
     top: 0;
     left: 0;
-    width: 50px;
+    width: 80px;
     padding: 10px;
     min-height: calc(100vh - 20px);
-    background-color: rgba($color: #81B121, $alpha: .8);
-    border: solid #fff;
+    background-color: rgba(40, 55, 129, 0.9);
+    border: solid rgba(255, 255, 255, 1);
     border-width: 0 1px 0 0;
     z-index: 999;
     transition: all .5s ease-in-out;
+
     .control {
+      margin: 20px 0;
       display: flex;
       justify-content: center;
       align-items: center;
       width: 50px;
       margin-bottom: 10px;
+
       i {
-        font-size: 2rem;
+        color: #9e9e9e;
+        font-size: 30px;
         cursor: pointer;
         transition: all .5s ease-in-out;
       }
     }
+
     &.show {
-      width: 180px;
+      width: 200px;
+
       .control > i {
         color: #fff;
         transform: rotateZ(-180deg);
       }
-      .navigation-icons {
-        color: #fff;
+
+      .navigation-icons i{
+        color: #fff ;
+      }
+
+      .logo img {
+        transform: rotate(270deg);
+        transition: all .5s ease-in-out;
       }
     }
+
     .navigation-icons {
       display: flex;
       justify-content: center;
@@ -91,30 +110,38 @@ export default {
       flex-direction: column;
       width: 50px;
       float: left;
+
       i {
-        font-size: 2rem;
+        color: #9e9e9e;
+        font-size: 1.8rem;
         padding: 10px 0;
         cursor: pointer;
         transition: all .5s ease-in-out;
+
         &:hover {
           color: #fff;
         }
       }
     }
+
     .navigation-links {
       padding-top: 14px;
       float: left;
+
       div {
-        font-size: 1.35rem;
+        color: #9e9e9e;
+        font-size: 1.3rem;
         padding-left: 10px;
         margin-bottom: 18px;
         cursor: pointer;
+
         &:hover {
           color: #fff;
         }
       }
     }
   }
+
   @mixin nav-childs($values...) {
     @each $var in $values {
       &:nth-child(#{$var}) {
@@ -122,10 +149,29 @@ export default {
       }
     }
   }
+
   .fade-enter-active, .fade-leave-active {
-    @include nav-childs(1,2,3,4,5);
+    @include nav-childs(1, 2, 3, 4, 5);
   }
+
   .fade-enter, .fade-leave-to {
     transform: scale(0);
+  }
+
+  .logo {
+    margin: 30px 0 20px 0;
+
+    hr {
+
+      padding-top: 20px;
+      border-bottom: 1px solid #e0e0e0;
+    }
+
+    img {
+      height: 40px;
+      width: 40px;
+      transform: rotate(15deg);
+      transition: all .5s ease-in-out;
+    }
   }
 </style>
